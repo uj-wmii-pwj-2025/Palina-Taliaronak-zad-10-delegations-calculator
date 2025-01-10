c99050e4abf0ebdb540e9f15a7bb0247a4ae5e19
Arkadiusz Sokołowski <arsokolowski@MacBook-Pro-4.local>
2025-01-10 15:44:46 +0100

    Initial commit

---
+++ src/main/java/uj/wmii/pwj/delegations/Calc.java
@@ -0,0 +1,14 @@
+package uj.wmii.pwj.delegations;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Calc {
+
+    BigDecimal calculate(String name, String start, String end, BigDecimal dailyRate) throws IllegalArgumentException {
+        return null;
+    }
+}
